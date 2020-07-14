ceb92755a29b103ea6adaaede2ef167117f3646c
Brad Chen <ctchen01@hotmail.com>
2020-07-14 18:52:10 +0800

    gitignore fix

--- PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/Security/Logins and Password/Password_Generator.sql
+++ PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/Security/Logins and Password/Password_Generator.sql
@@ -25,3 +25,22 @@ end
 
 select @output;
 go
+
+
+-- Method 2
+DECLARE @char CHAR = ''
+DECLARE @charI INT = 0
+DECLARE @password VARCHAR(100) = ''
+DECLARE @len INT = 12 -- Length of Password
+WHILE @len > 0
+BEGIN
+	SET @charI = ROUND(RAND()*100,0)
+	SET @char = CHAR(@charI)
+ 
+	IF @charI > 48 AND @charI < 122
+	BEGIN
+		SET @password += @char
+		SET @len = @len - 1
+	END
+END
+SELECT @password [PassWord]
