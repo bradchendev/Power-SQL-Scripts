047118c2c921fa5697ce186d9b59bbac65889eef
Brad Chen <ctchen01@hotmail.com>
2020-09-14 11:54:04 +0800

    modify

--- PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/04_Database/Size/uspGetDiskFreeSizeSendemailtoDBA_20200831.sql
+++ PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/04_Database/Size/uspGetDiskFreeSizeSendemailtoDBA_20200831.sql
@@ -1,12 +1,23 @@
+USE [DBA]
+GO
+
+/****** Object:  StoredProcedure [dbo].[uspGetDiskFreeSizeSendemailtoDBA]    Script Date: 2020/9/14 上午 11:28:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
 -- =============================================
 -- Author:        Brad Chen
--- Create date:    2020/8/31 
+-- Create date:    2020/9/14 
 -- Description:    檢查磁碟機可用空間
 -- Modified By    Modification Date    Modification Description
--- EXEC dbo.uspGetDiskFreeSizeSendemailtoDBA @recipientsList = 'bradchen@systex.com'
--- EXEC dbo.uspGetDiskFreeSizeSendemailtoDBA @recipientsList = 'bradchen@systex.com;ethancheng@systex.com'
+-- EXEC dbo.uspGetDiskFreeSizeSendemailtoDBA @ThresholdFreeSizeMB=1024,@recipientsList = 'bradchen@systex.com'
+-- EXEC dbo.uspGetDiskFreeSizeSendemailtoDBA @ThresholdFreeSizeMB=1024,@recipientsList = 'bradchen@systex.com;ethancheng@systex.com'
 -- =============================================
 ALTER procedure [dbo].[uspGetDiskFreeSizeSendemailtoDBA]
+@ThresholdFreeSizeMB int, 
 @recipientsList varchar(500)
 AS
 SET NOCOUNT ON;
@@ -54,6 +65,8 @@ set [Server] = N'D18DWDBA01'
 where [server] is null;
 
 
+DECLARE @AlertDiskCnt int = 0;
+
 SELECT @MaxRowID = MAX(RowID) FROM @disk
 WHILE @RowID <= @MaxRowID
 BEGIN
@@ -63,14 +76,16 @@ BEGIN
 		@FreeSize = [FreeSize(MB)] 
 	from @disk where RowID = @RowID
    
-	If  @FreeSize < 1024 and @drive not in ('Q','K')
+	--If  @FreeSize < 1024 and @drive not in ('Q','K')
+	If  @FreeSize < @ThresholdFreeSizeMB 
 	BEGIN  
 		-- 小於1G
-	  SET  @htmlTable = @htmlTable + N'<tr style="font-weight: bold; color: red;"><td>' + @Server + N'</td><td>' + @drive + N'</td><td align="right">'+ CAST(@FreeSize as nvarchar) + N'</td><td>可用空間小於1GB</td></tr>'
+		SET @AlertDiskCnt = @AlertDiskCnt + 1;
+		SET @htmlTable = @htmlTable + N'<tr style="font-weight: bold; color: red;"><td>' + @Server + N'</td><td>' + @drive + N'</td><td align="right">'+ CAST(@FreeSize as nvarchar) + N'</td><td>可用空間小於 ' + CAST(@ThresholdFreeSizeMB as nvarchar) + ' MB </td></tr>'
 	END
 	ELSE
 	BEGIN  
-	SET  @htmlTable = @htmlTable + N'<tr ><td>' + @Server + N'</td><td>' + @drive + N'</td><td align="right"> '+ REPLACE(CONVERT(NVARCHAR,CONVERT(MONEY,@FreeSize),1), '.00','') + N'</td><td>&nbsp;</td></tr>'
+		SET @htmlTable = @htmlTable + N'<tr ><td>' + @Server + N'</td><td>' + @drive + N'</td><td align="right"> '+ REPLACE(CONVERT(NVARCHAR,CONVERT(MONEY,@FreeSize),1), '.00','') + N'</td><td>&nbsp;</td></tr>'
 	END
 
     SET @RowID = @RowID + 1;
@@ -79,12 +94,18 @@ END
 SET @htmlbody =  N'<html>'
 SET @htmlbody =  @htmlbody + N'檢查主機: ' + @@SERVERNAME + '<p>'
 SET @htmlbody =  @htmlbody + N'檢查時間: ' + CONVERT(nvarchar(23), GETDATE(), 121) + '<p>'
-SET @htmlbody =  @htmlbody + N'如果可用空間小於1G並且磁碟代號不是Q或K則會出現紅色字體<p>'
+--SET @htmlbody =  @htmlbody + N'如果可用空間小於1G並且磁碟代號不是Q或K則會出現紅色字體<p>'
+SET @htmlbody =  @htmlbody + N'檢查值: ' + CAST(@ThresholdFreeSizeMB as nvarchar) + ' MB (低於此值下表會呈現紅色字體)<p>'
 SET @htmlbody =  @htmlbody + N'<table border="1">' + @htmlTable +  N'</table></html>'
 
 
 --SET @emailTitle = 'Disk Free size - [' + @@SERVERNAME + ']';
 SET @emailTitle = 'D18 SQL Server Disk Free size';
+
+IF @AlertDiskCnt > 0
+BEGIN
+	SET @emailTitle = @emailTitle + N' 有 ' + CAST(@AlertDiskCnt as nvarchar) + N' 個磁碟低於檢查值';
+END;
 
 --SELECT @htmlbody
 
@@ -96,3 +117,5 @@ SET @emailTitle = 'D18 SQL Server Disk Free size';
       @body = @htmlbody
 
 GO
+
+
