1f52b560e594b5f89bdfc5cf048a10688531455b
Brad Chen <ctchen01@hotmail.com>
2020-11-10 11:03:32 +0800

    add two files

---
+++ PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/04_Database/Size/DB_SpaceUsed.sql
@@ -0,0 +1,21 @@
+﻿
+
+USE AdventureWorks2017;  
+GO  
+EXEC sp_spaceused;  
+GO
+
+--database_name	database_size	unallocated space
+--AdventureWorks2017	336.00 MB	57.19 MB
+
+--reserved	data	index_size	unused
+--211776 KB	97384 KB	87264 KB	27128 KB
+
+
+
+USE AdventureWorks2017;  
+GO  
+EXEC sp_spaceused N'Purchasing.Vendor';  
+GO
+--name	rows	reserved	data	index_size	unused
+--Vendor	104                 	272 KB	16 KB	32 KB	224 KB
---
+++ PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/04_Database/Size/Log_Spaceused.sql
@@ -0,0 +1,13 @@
+﻿
+
+
+DBCC SQLPERF(LOGSPACE);
+
+
+--Database Name	Log Size (MB)	Log Space Used (%)	Status
+--master	1.992188	35.4902	0
+--tempdb	7.992188	11.14369	0
+--model	7.992188	6.304986	0
+--msdb	8.179688	16.38013	0
+--AdventureWorks2017	71.99219	10.48833	0
+
