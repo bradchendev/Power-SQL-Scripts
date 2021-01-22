3c345e5f590c80df06232b6cb17bea7fafdbd203
Brad Chen <ctchen01@hotmail.com>
2021-01-22 16:37:34 +0800

    add file for linked server

---
+++ PowerSQLScripts/PowerSQLScripts/PowerSQLScripts/SqlServer/Linked Server/LinkedServerProvider.sql
@@ -0,0 +1,8 @@
+﻿
+
+--安裝Third-party Provider (例如Oracle Provider)
+EXEC master.dbo.xp_enum_oledb_providers;
+
+--SQL 2014 not support Linked server to SQL 2000 workaround
+--安裝並使用SQL Server Native Client 10.0
+--或使用ODBC DSN
